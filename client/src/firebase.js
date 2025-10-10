a5980caed196485e887e75d6f979629a9ae8e82f
abhishekkumrawat95 <abhishekkumrawat95@gmail.com>
2025-10-11 01:39:52 +0530

    storagebucket

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAM6oTK6tmdKEINW_mTj8V-zH-q8HG6q6A",
   authDomain: "achat-app-6e714.firebaseapp.com",
   projectId: "achat-app-6e714",
-  storageBucket: "achat-app-6e714.appspot.com",
+  storageBucket: "achat-app-6e714.firebasestorage.app",
   messagingSenderId: "1078644008003",
   appId: "1:1078644008003:web:6c2b9b960960715054a0f6",
   measurementId: "G-R479F3BJ0P"
