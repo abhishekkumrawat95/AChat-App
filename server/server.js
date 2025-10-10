151dd9c10a8a22fb099dc3b91622803ac8d97dff
abhishekkumrawat95 <abhishekkumrawat95@gmail.com>
2025-10-11 00:29:29 +0530

    er

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://akchatcc.vercel.app/"], // Apna Vercel URL yahan daalein
+    origin: ["http://localhost:3000", "https://akchatcc.vercel.app"], // Apna Vercel URL yahan daalein
     methods: ["GET", "POST"],
   },
 });
